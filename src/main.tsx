2bc3b7419bec426ebf7331fdc4794d9624849666
Azizov S <azizovshorazi@gmail.com>
2024-02-23 19:51:09 +0500

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
+import {
+	FluentProvider,
+	teamsLightTheme,
+	tokens,
+} from '@fluentui/react-components'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './app/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<>
-		<App />
+		<FluentProvider
+			theme={teamsLightTheme}
+			style={{ backgroundColor: tokens.colorBrandBackground2 }}
+		>
+			<App />
+		</FluentProvider>
 	</>
 )
